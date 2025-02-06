890f2cff6fe472eda6e17313384c09c92746a6aa
brunocmnz <117315412+brunocmnz@users.noreply.github.com>
2025-02-06 20:30:49 -0300

    Update deletes-ok

--- facilita-gestao-app/src/App.js
+++ facilita-gestao-app/src/App.js
@@ -1,13 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Home from "./components/paginas/Home";
-import PetList from "./components/paginas/pet/PetList";
 import Container from "react-bootstrap/Container";
-import PetIncluir from "./components/paginas/pet/PetIncluir";
-import PetAlterar from "./components/paginas/pet/PetAlterar";
-import ServicoList from "./components/paginas/servico/ServicoList";
-import ServicoIncluir from "./components/paginas/servico/ServicoIncluir";
-import ServicoAlterar from "./components/paginas/servico/ServicoAlterar";
 import ProdutoList from "./components/paginas/produto/ProdutoList";
 import ProdutoIncluir from "./components/paginas/produto/ProdutoIncluir";
 import ProdutoAlterar from "./components/paginas/produto/ProdutoAlterar";
