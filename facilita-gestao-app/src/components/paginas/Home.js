e06f847dfc7845d7368eece45817aa4f4f2f6d1d
brunocmnz <117315412+brunocmnz@users.noreply.github.com>
2025-02-09 17:24:06 -0300

    Update Home

--- facilita-gestao-app/src/components/paginas/Home.js
+++ facilita-gestao-app/src/components/paginas/Home.js
@@ -1,6 +1,37 @@
 function Home(){
     return(
-        <h1 className={"colorWhite"}>Facilita Gestão. By Bruno e Leander.</h1>
+        <div>
+
+            <h1 className={"colorWhite"}>Facilita Gestão</h1>
+
+            <div className="introducao colorWhite" >
+                <h2>
+                    Proposta do sistema
+                </h2>
+                <p>
+                    A ideia do Facilita Gestão surgiu com um trabalho de Programação Web, onde o intuito era desenvolver
+                    um sitema que abordasse os conceitos estudados e tivesse algum propósito interessante.
+                </p>
+                <p>
+                    Pensando nisso, foi criado um sistema para gerenciar o estoque de um estabelecimento e também controlar
+                    a parte de contas a pagar, pagas ou vencidas. Na parte de produtos, é possível inserir e remover um produto
+                    além de poder alterar sua quantidade de estoque.
+                </p>
+                <p>
+                    Uma funcionalidade interessante é que há um alerta sobre qual o estado do produto, utilizando a
+                    quantidade mínima de estoque, em que é possível saber se a quantidade está ok, próximo do limite,
+                    abaixo do limite, ou esgotado.
+                </p>
+
+                <h2>
+                    Controle de estoque
+                </h2>
+
+                <h2>
+                    Gestão de contas a pagar
+                </h2>
+            </div>
+        </div>
     );
 }
 
